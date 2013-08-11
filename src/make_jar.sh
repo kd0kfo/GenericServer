fd64138b451d21b1af4c7ac13c14b92cf4831c21
David Coss <david@davecoss.com>
2013-08-11 10:10:31 -0500

    Changed make jar to include json-simple classes. Simply unzip json simple jar in the src/ directory and run make_jar.sh

--- src/make_jar.sh
+++ src/make_jar.sh
@@ -1,2 +1,2 @@
 (cd com/davecoss/android/genericserver/ && javac -cp ~/src/json-simple-1.1.1.jar {GenericServer,ServerBundle,Standalone}.java)
-jar cfm GenericServer.jar Manifest.txt com
+jar cfm GenericServer.jar Manifest.txt com org
