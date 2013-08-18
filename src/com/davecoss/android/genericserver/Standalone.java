de229cf5cb66e23ab7426240155df8fc91096471
David Coss <david@davecoss.com>
2013-08-18 14:44:26 -0500

    Reformatted Standalone.java

--- src/com/davecoss/android/genericserver/Standalone.java
+++ src/com/davecoss/android/genericserver/Standalone.java
@@ -8,70 +8,57 @@ import java.io.IOException;
 public class Standalone {
 
 	public static void main(String[] args) throws IOException {
-	    ServerBundle server = new ServerBundle();    
+		ServerBundle server = new ServerBundle();
 		Console console = System.console();
 		PrintWriter cout = console.writer();
 
 		server.start_server();
 		cout.println("Server is running on port " + server.get_port());
-		
+
 		String input;
-		while((input = console.readLine(">")) != null)
-		    {
-			if(input.equals("stop"))
-			    {
+		while ((input = console.readLine(">")) != null) {
+			if (input.equals("stop")) {
 				break;
-			    }
-			else if(input.equals("getaddr"))
-			    {
+			} else if (input.equals("getaddr")) {
 				String addr = server.get_address();
-				if(addr == null)
-				    addr = "Not connected";
+				if (addr == null)
+					addr = "Not connected";
 				cout.println(addr);
-			    }
-			else if(input.equals("setaddr"))
-			    {
+			} else if (input.equals("setaddr")) {
 				String addr = console.readLine("What address? ");
 				try {
-				    server.stop_server();
+					server.stop_server();
 				} catch (InterruptedException ie) {
-				    cout.println("Error ending server: " + ie.getMessage());
+					cout.println("Error ending server: " + ie.getMessage());
 				}
 				try {
-				    server.start_server(addr);
-				    cout.println("Server is running on port " + server.get_port());
+					server.start_server(addr);
+					cout.println("Server is running on port "
+							+ server.get_port());
 				} catch (UnknownHostException uhe) {
-				    cout.println("Unknown host: " + addr);
+					cout.println("Unknown host: " + addr);
 				}
 
-					    
-			    }
-			else if(input.equals("getdir"))
-			    {
+			} else if (input.equals("getdir")) {
 				cout.println(server.getdir());
-			    }
-			else if(input.equals("setdir"))
-			    {
+			} else if (input.equals("setdir")) {
 				String dir = console.readLine("What directory? ");
 				server.setdir(dir);
-			    }
-			else if(input.equals("status"))
-			    {
-				if(server == null || !server.is_running())
-				    cout.println("Closed");
+			} else if (input.equals("status")) {
+				if (server == null || !server.is_running())
+					cout.println("Closed");
 				else
-				    cout.println("Listening on " + server.get_address() + ":" + server.get_port());
-			    }
-			else
-			    {
+					cout.println("Listening on " + server.get_address() + ":"
+							+ server.get_port());
+			} else {
 				cout.println("Unknown: " + input);
-			    }
-		    }
+			}
+		}
 		cout.println("Stopping");
 		try {
-		    server.stop_server();
+			server.stop_server();
 		} catch (InterruptedException ie) {
-		    cout.println("Error ending server: " + ie.getMessage());
+			cout.println("Error ending server: " + ie.getMessage());
 		}
 	}
 
