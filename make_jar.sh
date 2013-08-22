89564c2f05648be51fa66b93e12996773ce5cfb6
David Coss <david@davecoss.com>
2013-08-21 20:58:41 -0500

    Added missing file.

---
+++ make_jar.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd src
+CLASSPATH=.:$(pwd)
+(cd com/davecoss/android/genericserver/ && javac -cp $CLASSPATH {GenericServer,ServerBundle,Standalone}.java) && jar cfm GenericServer.jar Manifest.txt com org && mv GenericServer.jar ..
