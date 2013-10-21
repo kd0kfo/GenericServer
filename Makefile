c942ec8c5222cd1561ea8a8f1bc0e56334b8e79a
David Coss <david@davecoss.com>
2013-10-21 14:19:15 -0500

    Converted to ant.

--- Makefile
+++
@@ -1,9 +0,0 @@
-
-all:
-	./make_jar.sh
-
-clean:
-	rm -f GenericServer.jar src/com/davecoss/android/genericserver/*.class
-
-%.jks:
-	keytool -genkey -keystore $@ -keyalg RSA
--- make_jar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd src
-echo build_date=$(date +"%Y-%m-%d %H:%M") > com/davecoss/android/genericserver/build.info
-CLASSPATH=.:$(pwd)
-(cd com/davecoss/android/genericserver/ && javac -cp $CLASSPATH $(\ls *.java|grep -v Console) ) && jar cfm GenericServer.jar Manifest.txt com org && mv GenericServer.jar ..
